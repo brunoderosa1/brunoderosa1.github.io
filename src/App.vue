f42edf2c73f68828992194fa4c6a65fb65a043b0
brunoderosa1 <brunoderosa180@gmail.com>
2022-05-10 00:24:54 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="app">
+    <div class="cage">
+      <h1 class="title">Visualizador de PDF</h1>
+    <p class="paragraph-1">Al presionar el boton, se abrira una ventana superpuesta donde se visualizara un archivo PDF utilizando un elemento iframe</p>
+    <button class="btn1" @click="iframeViewer" v-if="!iframeIsVisible">Presione para ver</button>
+    <button class="btn2" @click="iframeViewer" v-else>Presione para ocultar</button>
+    </div>
+    <PDFViewer v-if='iframeIsVisible' />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script>
+import PDFViewer from '@/components/PDFViewer.vue'
+
+export default {
+  components: { PDFViewer },
+  data () {
+    return {
+      iframeIsVisible: false
+    }
+  },
+  methods: {
+    iframeViewer () {
+      this.iframeIsVisible = !this.iframeIsVisible
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cage {
+  display: flex;
+  flex-direction: column;
+}
+.title {
+  font-family: Helvetica, sans-serif;
+  color:rgb(29, 152, 83);
   text-align: center;
-  color: #2c3e50;
+  font-weight: 600;
+  text-shadow: 1mm;
+}
+.paragraph-1 {
+  font-family: Arial, Helvetica, sans-serif;
+  color:rgb(29, 152, 83);
+  text-align: center;
+}
+.btn1 {
+  color: rgb(221, 44, 44);
+  align-items: center ;
+}
+.btn1:hover {
+  color:turquoise;
+  animation: loop 1s infinite;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+@keyframes loop {
+  0% { background-color: orange; }
+  25% { background-color: orangered; }
+  50% { background-color: orange; }
+  75% {background-color: orangered;}
+  100%{ background-color: orange;}
 }
 </style>
---
+++ src/components/PDFViewer.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+    <hr>
+    <iframe class="reader" src="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" frameborder="0"></iframe>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container {
+  display: block;
+  position:fixed;
+  bottom: 5%;
+  right: 5%;
+  left: 5%;
+}
+.reader {
+    width:100%;
+    height:30rem;
+}
+</style>
